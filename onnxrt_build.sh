2fcdd2916daafef9e65955bb2f1b2d99269dd34d
JuneChul Roh <jroh@ti.com>
2021-07-07 15:19:40 -0500

    Added script for downloading pre-built protobuf

--- onnxrt_build.sh
+++ onnxrt_build.sh
@@ -4,6 +4,12 @@
 # - onnxruntime/build/Linux/Release/libonnxruntime.so.1.7.0
 # - onnxruntime/build/Linux/Release/dist/onnxruntime_tidl-1.7.0-cp36-cp36m-linux_aarch64.whl
 
+# protobuf options:
+# a) in case of building protobuf from source
+# --path_to_protoc_exe $(pwd)/cmake/external/protobuf/src/protoc \
+# b) in case using pre-built protobuf
+# --path_to_protoc_exe $(pwd)/cmake/external/protoc-3.11.3-linux-aarch_64/bin/protoc \
+
 # update how many CPUs to use
 NPROC=8
 cd onnxruntime
