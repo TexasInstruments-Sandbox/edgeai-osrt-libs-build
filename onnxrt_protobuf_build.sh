40ad7a938f76ec37036766d1eef6296d9e338968
JuneChul Roh <jroh@ti.com>
2021-07-07 11:51:34 -0500

    Added a missing script, and minor updates

--- onnxrt_protobuf_build.sh
+++ onnxrt_protobuf_build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # This script should be run inside the CONTAINER
-# TODO: use pre-built, e.g., "protoc-3.11.2-linux-aarch64.zip" and just unzip
+# TODO: use pre-built, e.g., "protoc-3.11.3-linux-aarch64.zip" and just unzip
 cd onnxruntime
 cd cmake/external/protobuf && ./autogen.sh && ./configure && make
 cd -
