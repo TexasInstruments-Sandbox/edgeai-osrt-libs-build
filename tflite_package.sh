f21c6591e3a329e206c3a781fe4fc9beda82f5e4
JuneChul Roh <jroh@ti.com>
2021-08-13 06:24:43 -0500

    Added the resulting tarballs for TIDL 8.0 under tarballs folder

--- tflite_package.sh
+++ tflite_package.sh
@@ -4,8 +4,11 @@ TIDL_VER="tidl8.0.0"
 LIB_DIR=./tensorflow/tensorflow/lite/tools/make/gen/linux_aarch64/lib/
 INCLUDE_DIR=./tensorflow/tensorflow/lite/tools/make/downloads/flatbuffers/include
 DST_DIR=tensorflow-aarch64-ubuntu18.04
+TARBALL=$DST_DIR-$TIDL_VER.tar.gz
 rm -rf $DST_DIR
 mkdir -p $DST_DIR
 cp $LIB_DIR/libtensorflow-lite.a $DST_DIR
 cp -r $INCLUDE_DIR $DST_DIR
-tar czvf $DST_DIR-$TIDL_VER.tar.gz $DST_DIR
+tar czvf $TARBALL $DST_DIR
+mkdir -p tarballs
+mv $TARBALL tarballs
