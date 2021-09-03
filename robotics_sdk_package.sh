3157b2c0e47e5c92e09ef9d131f3af5cc305e6a8
JuneChul Roh <jroh@ti.com>
2021-09-03 18:39:52 -0500

    Add a packaging script for Robo SDK: robotics_sdk_package.sh. ADASVISION-4967

---
+++ robotics_sdk_package.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+## Robotics SDK 0.5 release: package into a tarball
+REL_VER="8.0"
+DST_DIR=dlrt-libs-aarch64-ubuntu18.04
+TARBALL=${DST_DIR}_${REL_VER}.tar.gz
+
+# mkdir
+rm -rf $DST_DIR
+mkdir -p $DST_DIR
+
+# ONNX
+LIB_DIR=./onnxruntime/build/Linux/Release
+cp $LIB_DIR/libonnxruntime.so.1.7.0 $DST_DIR
+
+# Tensorflow
+LIB_DIR=./tensorflow/tensorflow/lite/tools/make/gen/linux_aarch64/lib/
+cp $LIB_DIR/libtensorflow-lite.a $DST_DIR
+
+# make tarball
+tar czvf $TARBALL $DST_DIR
+mkdir -p tarballs
+mv $TARBALL tarballs
