8ca5b5ae7f1d01cf0ede61c76e83b2fcdeb2f161
JuneChul Roh <jroh@ti.com>
2024-06-04 11:54:27 -0500

    Updated collect_libs.sh

--- scripts/collect_libs.sh
+++ scripts/collect_libs.sh
@@ -23,15 +23,15 @@ lib_files=(
     # $ROOT_DIR/home/root/vision-apps-build/workarea/vision_apps/out/J784S4/A72/LINUX/release/libtivision_apps.so.9.2.0
     $ROOT_DIR/home/root/vision-apps-build/workarea/vision_apps/out/J784S4/A72/LINUX/release/libti-vision-apps-j784s4_9.2.0-ubuntu22.04.deb
     # ONNX
-    # $ROOT_DIR/home/root/dlrt-build/workarea/onnxruntime/build/Linux/Release/libonnxruntime.so.1.14.0
-    $ROOT_DIR/home/root/dlrt-build/workarea/onnxruntime/build/Linux/Release/dist/onnxruntime_tidl-1.14.0-cp310-cp310-linux_aarch64.whl
-    $ROOT_DIR/home/root/dlrt-build/workarea/onnx-1.14.0-ubuntu22_aarch64.tar.gz
+    # $ROOT_DIR/home/root/osrt-build/workarea/onnxruntime/build/Linux/Release/libonnxruntime.so.1.14.0
+    $ROOT_DIR/home/root/osrt-build/workarea/onnxruntime/build/Linux/Release/dist/onnxruntime_tidl-1.14.0-cp310-cp310-linux_aarch64.whl
+    $ROOT_DIR/home/root/osrt-build/workarea/onnx-1.14.0-ubuntu22_aarch64.tar.gz
     # TFLite
-    # $ROOT_DIR/home/root/dlrt-build/workarea/tensorflow/tflite_build/libtensorflow-lite.a
-    $ROOT_DIR/home/root/dlrt-build/workarea/tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/python3/dist/tflite_runtime-2.12.0-cp310-cp310-linux_aarch64.whl
-    $ROOT_DIR/home/root/dlrt-build/workarea/tflite-2.12-ubuntu22_aarch64.tar.gz
+    # $ROOT_DIR/home/root/osrt-build/workarea/tensorflow/tflite_build/libtensorflow-lite.a
+    $ROOT_DIR/home/root/osrt-build/workarea/tensorflow/tensorflow/lite/tools/pip_package/gen/tflite_pip/python3/dist/tflite_runtime-2.12.0-cp310-cp310-linux_aarch64.whl
+    $ROOT_DIR/home/root/osrt-build/workarea/tflite-2.12-ubuntu22_aarch64.tar.gz
     # DLR
-    $ROOT_DIR/home/root/dlrt-build/workarea/neo-ai-dlr/python/dist/dlr-1.13.0-py3-none-any.whl
+    $ROOT_DIR/home/root/osrt-build/workarea/neo-ai-dlr/python/dist/dlr-1.13.0-py3-none-any.whl
 )
 
 for lib_file in "${lib_files[@]}"; do
